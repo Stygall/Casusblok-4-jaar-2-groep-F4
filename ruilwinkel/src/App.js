e672b93b583ae4842417946f710caeaa03efab6e
Max Smeets <1959514smeets@zuyd.nl>
2021-05-21 13:30:18 +0200

    initial commit

---
+++ ruilwinkel/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>Product registratie</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ ruilwinkel/src/components/product/product.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function header(){
+    return(
+        <div className="App-header">
+            <h1>Product registratie</h1>
+        </div>
+    )
+}
