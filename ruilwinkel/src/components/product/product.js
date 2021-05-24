7f437ae2cfd3b4c670217033d243a5e940a40bf6
Max Smeets <1959514smeets@zuyd.nl>
2021-05-24 11:39:25 +0200

    productform v3 + styling

--- ruilwinkel/src/components/product/product.js
+++ ruilwinkel/src/components/product/product.js
@@ -6,35 +6,52 @@ function Product(){
         <div>
             <form className="productForm">
                 <label>
-                    Artikelnummer: 
-                    <input  placeholder="Artikelnummer" className="input" type="number" min="0" name="artNum" required/><br/>
+                    <p className="text">
+                        Artikelnummer:<br/>
+                        <input  placeholder="Artikelnummer" className="input" type="number" min="0" name="artNum" required/><br/>
+                    </p> 
+                    
                 </label>
                 <label>
-                    Productnaam:
-                    <input placeholder="Productnaam" className="input" type="text" name="prodName" required/><br/>
+                    <p className="text">
+                        Productnaam:<br/>
+                        <input placeholder="Productnaam" className="input" type="text" name="prodName" required/><br/>
+                    </p>                  
+                    
                 </label>
                 <label>
-                    Categorie:
-                    <select placeholder="Categorie" className="input" type="text" name="categorie" required>
-                        <option value="Laptop">Laptop</option>
-                        <option value="Randapparatuur">Randapparatuur</option>
-                        <option value="Decoratie">Decoratie</option>
-                        <option value="Spellen analoog">Spellen analoog</option>
-                        <option value="Spellen digitaal">Spellen digitaal</option>
-                        <option value="Keukengerei">Keukengerei</option>
-                    </select><br/>
+                    <p className="text">
+                        Categorie:<br/>
+                        <select placeholder="Categorie" className="input" type="text" name="categorie" required>
+                            <option value="Laptop">Laptop</option>
+                            <option value="Randapparatuur">Randapparatuur</option>
+                            <option value="Decoratie">Decoratie</option>
+                            <option value="Spellen analoog">Spellen analoog</option>
+                            <option value="Spellen digitaal">Spellen digitaal</option>
+                            <option value="Keukengerei">Keukengerei</option>
+                        </select><br/>
+                    </p>                                      
                 </label>
                 <label>
-                    Credits:
-                    <input placeholder="Credits" className="input" type="number" min="0" name="credits" required/><br/>
+                    <p className="text">
+                        Credits:<br/>
+                        <input placeholder="Credits" className="input" type="number" min="0" name="credits" required/><br/>
+                    </p>                   
                 </label>
                 <label>
-                    Aantal:
-                    <input placeholder="Aantal" className="input" type="number" min="1" name="amount" required/><br/>
+                    <p className="text">
+                        Aantal:<br/>
+                        <input placeholder="Aantal" className="input" type="number" min="1" name="amount" required/><br/>
+                    </p>
+                    
+                    
                 </label>
-
-                <input className="input" type="submit" value="Product toevoegen"/>
+                <label>
+                    <input className="input" type="submit" value="Product toevoegen"/>
+                </label>
+                
             </form>
+            
         </div>
     )
 }
