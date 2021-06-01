37b1678353d9b7dad6cd84cb377d023ead87d847
Max Smeets <71692651+MaxSmeets@users.noreply.github.com>
2021-06-01 19:10:01 +0200

    footer component v2

--- ruilwinkel/src/components/appFooter/footer.js
+++ ruilwinkel/src/components/appFooter/footer.js
@@ -5,20 +5,19 @@ import "./footer.css";
 function Footer() {
   return (
     <div className="footer-styling">
-      <h3>Contact</h3>
-      <p>
+      <h3 className="footer-text">Contact</h3>
+      <p className="footer-text">
         <b>Adres: </b> Kerkstraat 46, Vaals
         <br />
         <b>Tel: </b> +31 6 20 74 98 86
       </p>
-      <p>
+      <p className="footer-text">
         <h5>Openingstijden:</h5>
         <li>Dinsdag 10.00-14.00</li>
         <li>Woensdag 12.00-16.00</li>
         <li>Donderdag 10.00-14.00</li>
         <li>Elke eerste zaterdag van de maand 11.00-15.00</li>
       </p>
-      <img src="RuilwinkelLogo.png" />
     </div>
   );
 }
