0ed3b71ee25a1b7b2171a3d46e35334ac6dc3a9d
Stygall <59049395+Stygall@users.noreply.github.com>
2021-06-11 15:23:14 +0200

    fixes

--- ruilwinkel/gulpfile.js
+++
@@ -1,8 +0,0 @@
-﻿var surge = require('gulp-surge')
-
-gulp.task('deploy', [], function () {
-    return surge({
-        project: './build',         // Path to your static build directory
-        domain: 'ruilwinkel2.surge.sh'  // Your domain or Surge subdomain
-    })
-})
